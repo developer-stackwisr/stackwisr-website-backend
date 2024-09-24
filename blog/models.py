2ca6b12f78b959fcf26d664e63ef7b467ab546f9
cxstackwisr <ehiremengold@gmail.com>
2024-09-24 15:38:17 +0100

    modified author field in blog model

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from .validators import validate_image
 class Blog(models.Model):
   image = models.ImageField(upload_to='blog_images/', blank=True, null=True, validators=[validate_image])
   slug = models.SlugField(unique=True, max_length=160, blank=True)
-  author = models.CharField(help_text='Author name', max_length=50)
+  author = models.CharField(help_text='Author\'s name', max_length=50)
   title = models.CharField(max_length=250)
   body = RichTextField()
   created = models.DateField(auto_now_add=True)
