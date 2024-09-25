02c4405ea97cac9f8e247ce06da7d7ffe7cd8536
cxstackwisr <ehiremengold@gmail.com>
2024-09-25 18:58:03 +0100

    using decouple for enviroment variables

--- backend/settings.py
+++ backend/settings.py
@@ -21,14 +21,14 @@ load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
+from decouple import config
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 print(ALLOWED_HOSTS)
 
