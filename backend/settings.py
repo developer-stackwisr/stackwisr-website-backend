f5af29ab4eb01495f301cb16a574c64e178518bd
cxstackwisr <ehiremengold@gmail.com>
2024-09-26 11:45:58 +0100

    adding staticfiles_dirs to settings.py and added db.sqlite3 to .gitignore

--- backend/settings.py
+++ backend/settings.py
@@ -132,6 +132,11 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # or wherever your static files are located
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
