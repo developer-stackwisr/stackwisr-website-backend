d53e0140aa4d0bdb58e5a3a343268471669b3f33
cxstackwisr <ehiremengold@gmail.com>
2024-09-27 10:33:43 +0100

    changed debug to true

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ from decouple import config
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','167.172.61.220']
 
