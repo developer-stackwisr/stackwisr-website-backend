b334079749142d76a9adbe9aa09006bfe3d2c72a
cxstackwisr <ehiremengold@gmail.com>
2024-09-27 01:31:42 +0100

    working on staticfiles path in settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -130,6 +130,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')  
 
+STATICFILES_DIRS  = [
+  os.path.join(BASE_DIR, 'static')  
+]
 
 
 # Default primary key field type
