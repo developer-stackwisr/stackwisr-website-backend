4c99e35665d46863957a2d0b28b83970a81c8ed4
cxstackwisr <ehiremengold@gmail.com>
2024-10-07 17:21:34 +0100

    added api route to allowed_hosts

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','167.172.61.220']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','167.172.61.220',  "api.stackwisr.co.uk"]
 
 # Application definition
 
