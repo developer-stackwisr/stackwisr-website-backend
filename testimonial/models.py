5d21a68430188d8453b4b338d7cc22e04d04114a
cxstackwisr <ehiremengold@gmail.com>
2024-10-16 08:02:38 +0100

    made image field in testimonial nullable

--- testimonial/models.py
+++ testimonial/models.py
@@ -4,7 +4,7 @@ class Testimonial(models.Model):
   name = models.CharField(max_length=150, help_text='Testifier\'s name')
   job_description = models.CharField(max_length=250)
   feedback = models.TextField()
-  image= models.ImageField(upload_to='testifier-image')
+  image= models.ImageField(upload_to='testifier-image', blank=True, null=True)
 
   def __str__(self):
     return self.feedback
