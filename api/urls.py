d3af2fba801e81f9426667f649d47dca09c54738
cxstackwisr <ehiremengold@gmail.com>
2024-10-17 08:27:35 +0100

    configured testimonies api route

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import BlogAPIView, CareerPathAPIView, LatestBlogPostsAPIView
+from .views import BlogAPIView, CareerPathAPIView, LatestBlogPostsAPIView, TestimonialAPIView
 
 urlpatterns = [
   path('blog/posts/', BlogAPIView.as_view()),
   path('blog/post/<str:slug>/', BlogAPIView.as_view()),
   path('blog/latest-posts/', LatestBlogPostsAPIView.as_view()),
+  path('testimonies/', TestimonialAPIView.as_view(), name='testimonies'),
   path('careerpaths/', CareerPathAPIView.as_view(), name='careerpath-list'),
   path('careerpaths/<slug:slug>/', CareerPathAPIView.as_view(), name='careerpath-detail'),
 ]
