6a98eef0b097efafea2ffb7fa93d388181eb1907
cxstackwisr <ehiremengold@gmail.com>
2024-10-18 11:21:36 +0100

    adding more message to the return of the leads api endpoint

--- api/views.py
+++ api/views.py
@@ -73,5 +73,5 @@ class LeadAPIView(APIView):
     serializer = LeadSerializer(data=request.data)
     if serializer.is_valid():
       serializer.save()
-      return Response(status=status.HTTP_201_CREATED)
-    return Response(status=status.HTTP_400_BAD_REQUEST)
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
